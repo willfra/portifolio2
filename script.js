e24c467d6f3efbd371d5347182b9de85bc1ab8e5
William França <williamkusanagy@gmail.com>
2021-09-09 01:10:59 -0300

    atualização script icon menu

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function () {
   document.querySelector(".container").classList.toggle("show-menu");
-  document.getElementById("icon-hamburguer").className = "fas fa-times";
 });
+
+document.getElementById("icon-hamburguer").className = "fas fa-times";
